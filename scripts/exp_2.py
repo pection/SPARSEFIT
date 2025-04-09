0e85080bc26d38432e8b1c54d5ece346ad2b8926
Naphat <naphat.mars@gmail.com>
2025-04-09 14:03:13 +0700

    update exp_2.py

--- scripts/exp_2.py
+++ scripts/exp_2.py
@@ -229,9 +229,7 @@ def run_exp(args):
                       'ecqa': '../data/ECQA-Dataset',
                       'sensemaking': '../data/SenseMaking/',
                       'sbic': '../data/SBIC/'}
-    testdir = os.path.join(os.getcwd(),'../data/SBIC')
-    print(f"now = {os.path.join(os.getcwd(),'../data/SBIC')}")
-    print(os.listdir(testdir))
+
     commands = []
     for hparams in itertools.product(dataset_vals,
                                     seed_vals,
