1fab85c72f96333df6c878fb04aaa055991ee4f7
Naphat <pection.naphat@gmail.com>
2025-04-14 10:29:45 +0700

    update input_to_label_and_rationale.py

--- scripts/input_to_label_and_rationale.py
+++ scripts/input_to_label_and_rationale.py
@@ -585,7 +585,7 @@ def main():
             # if 'layer_norm.weight' in name or "SelfAttention.q" in name:
             #     param.requires_grad = True
 
-            if 'layer_norm.weight' in name or "SelfAttention.v" in name:
+            if "SelfAttention.k" in name:
                 param.requires_grad = True
             else:
                 param.requires_grad = False
